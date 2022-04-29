017e3bdf3081c01d55c16bd48b4d998f378181ae
99 <hahahoho5915@gmail.com>
2022-04-29 21:15:30 +0900

    first commit

---
+++ src/main/java/com/kk99corn/alertUmbrella/AlertUmbrellaApplication.java
@@ -0,0 +1,13 @@
+package com.kk99corn.alertUmbrella;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AlertUmbrellaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AlertUmbrellaApplication.class, args);
+	}
+
+}
